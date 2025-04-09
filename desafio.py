ded8b15845855a39f4fb74b2376e369d6e6e7dba
KeityR <ranna.keity@gmail.com>
2025-04-09 00:43:45 -0300

    Criando as Funcoes

--- desafio.py
+++ desafio.py
@@ -47,10 +47,3 @@ def filtrar_produtos_nao_entregues(lista: list[dict]) -> list[dict]:
     return lista_produtos_filtrados
 
 
-lista_de_produtos = abrir_arquivo(path_arquivo)
-produtos_entregues = filtrar_produtos_nao_entregues(lista_de_produtos)
-soma_total = soma_dos_precos(lista_de_produtos)
-faturamento = faturamento_total(lista_de_produtos) 
-print(soma_total)
-print(f'{faturamento:.2f}')
-print(produtos_entregues)
